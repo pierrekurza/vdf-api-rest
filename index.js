38cfa93a80860d8bf04669e0bc037bdada9904cf
Pierre K <pierrekurzawski@gmail.com>
2023-10-05 09:43:50 +0200

    Update index.js

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ const db = mysql.createConnection({
 
 app.listen(PORT, function() {
     console.log(`Restful API is running on PORT ${PORT}`);
-   });
+});
 
 //Get all products from the database
 app.get('/getAllProducts', (req, res) => {
@@ -85,16 +85,16 @@ app.get("/getUserByID", (req, res, next) => {
 
 // Récupération des commandes pour un utilisateur
 app.get("/getOrdersByUser", (req, res, next) => {
-    db.query("SELECT * FROM order WHERE user_id = ?;", req.body.user_id, (error, result) => {
+    db.query("SELECT o.id_order, o.user_id, o.total_amount, os.name as 'order_status' FROM `order` o INNER JOIN order_status os ON o.id_order_status = os.id_order_status WHERE o.user_id = 1;", req.body.user_id, (error, result) => {
         if(error) {
             return res.status(500).json({ status: "ERROR", error});
         }
-        return res.json(data);
+        return res.json(result);
     });
 });
 
 
-
+// Créer une commande pour un utilisateur avec les produits associés
 app.post("/createOrderForUser", (req, res, next) => {
     let sumOrder = 0;
     if(req.body.products.length > 1){
@@ -106,15 +106,16 @@ app.post("/createOrderForUser", (req, res, next) => {
     }
 
     let lastID;
-    db.query("INSERT INTO `order` (user_id, total_amount, id_order_status) VALUES (?, ?, ?);", [req.body.user_id, sumOrder, 1], (error, result) => {
+    db.query("INSERT INTO `order` (user_id, total_amount, id_order_status) VALUES (?, ?, ?);",
+     [req.body.user_id, sumOrder, 1], (error, result) => {
         if(error) {
             return res.status(500).json({ status: "ERROR", error});
         }
         lastID = result.insertId;
         if(req.body.products.length > 1){
-            console.log("SuuS");
             for(let i = 0; i < req.body.products.length; i++){
-                db.query("INSERT INTO order_line (id_order, id_product, quantity) VALUES (?, ?, ?);", [lastID, req.body.products[i].id_product, req.body.products[i].quantity], (error, result) => {
+                db.query("INSERT INTO order_line (id_order, id_product, quantity) VALUES (?, ?, ?);",
+                 [lastID, req.body.products[i].id_product, req.body.products[i].quantity], (error, result) => {
                     if(error) {
                         return res.status(500).json({ status: "ERROR", error});
                     }
@@ -122,8 +123,8 @@ app.post("/createOrderForUser", (req, res, next) => {
                 });
             }
         } else {
-            console.log("JaaJ");
-            db.query("INSERT INTO order_line (id_order, id_product, quantity) VALUES (?, ?, ?);", [lastID, req.body.products[0].id_product, req.body.products[0].quantity], (error, result) => {
+            db.query("INSERT INTO order_line (id_order, id_product, quantity) VALUES (?, ?, ?);",
+             [lastID, req.body.products[0].id_product, req.body.products[0].quantity], (error, result) => {
                 if(error) {
                     return res.status(500).json({ status: "ERROR", error});
                 }
@@ -132,3 +133,5 @@ app.post("/createOrderForUser", (req, res, next) => {
         }
     });
 });
+
+app.get("/")
